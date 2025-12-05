9502aca591c76e53078eb8f0a74afa9d2209df9b
Talha Burak Şimşek <talha.simsek@live.acibadem.edu.tr>
2025-12-06 00:00:22 +0300

    fakülte app düzenlendi

--- faculty/admin.py
+++ faculty/admin.py
@@ -4,5 +4,4 @@ from faculty.models import Faculty
 # Register your models here.
 @admin.register(Faculty)
 class FacultyAdmin(admin.ModelAdmin):
-    list_display = ('full_name', 'dean_name')
-    search_fields = ('dean_name',)
+    list_display = ('full_name',)
--- faculty/models.py
+++ faculty/models.py
@@ -2,13 +2,6 @@ from django.db import models
 
 class Faculty(models.Model):
     full_name = models.CharField(max_length=100)
-    dean_name = models.ForeignKey(
-        'dean.Dean',
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name='faculties'
-    )
 
     def __str__(self):
         return self.full_name
